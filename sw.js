c25aac50200d8f9ef370b44b03856cd28af28369
ritampaine75-debug <ritampaine75@gmail.com>
2026-02-05 09:23:02 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'super-ai-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn-icons-png.flaticon.com/512/1698/1698535.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
